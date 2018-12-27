8e42f83ce89b84c2115dc842006cd92f8c5b0e5f
andela-fomokaro <faith.omokaro@andela.com>
2018-12-27 11:47:42 +0100

    retrieving user info

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -10,14 +10,20 @@ passport.use(
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 }, (accessToken, refreshToken, profile, done) => {
-  //passport callback function
-  console.log('passport callback fired');
-  console.log(profile);
-  new User({
-    username: profile.displayName,
-    googleID: profile.id
-    }).save().then((newUser) => {
-      console.log(`new user was created ${newUser}`);
-    });
-  })
-)
+  //check if user already exists in our db
+  User.findOne({googleID: profile.id}).then((currentUser) => {
+    if(currentUser) {
+      //already have the user
+      console.log(`user is ${currentUser}`)
+    }else {
+      //if not, create user in our db
+      new User({
+        username: profile.displayName,
+        googleID: profile.id
+        }).save().then((newUser) => {
+          console.log(`new user was created ${newUser}`);
+          });
+        }
+      });
+    })
+  )
