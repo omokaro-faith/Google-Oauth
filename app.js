a883a2225c43c0e91ba9f28745e9fa7c661e7975
andela-fomokaro <faith.omokaro@andela.com>
2018-12-27 10:34:33 +0100

    mongodb setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 const authRoutes = require('./routes/auth-routes');
 require('./config/passport-setup');
@@ -7,6 +8,11 @@ const app = express();
 
 //setup view engine
 app.set('view engine', 'ejs');
+
+//connect to mongodb
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true}, () => {
+  console.log(`connected to mongoDB`);
+});
 
 //create home route
 app.get('/', (req, res) => {
@@ -19,3 +25,4 @@ app.use('/auth', authRoutes);
 app.listen('3000', () => {
   console.log('Listening for requests on port 3000');
 });
+
