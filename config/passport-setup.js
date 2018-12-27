0b85cf85ae614672ae88a3e65942fdc805e1797e
andela-fomokaro <faith.omokaro@andela.com>
2018-12-27 11:33:01 +0100

    saving users to mongodb

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../models/user-model');
 
 
 passport.use(
@@ -12,5 +13,11 @@ passport.use(
   //passport callback function
   console.log('passport callback fired');
   console.log(profile);
+  new User({
+    username: profile.displayName,
+    googleID: profile.id
+    }).save().then((newUser) => {
+      console.log(`new user was created ${newUser}`);
+    });
   })
 )
