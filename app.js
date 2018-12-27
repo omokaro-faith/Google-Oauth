f76f5edcb7d1b471c2060164e7126af10c1300a4
andela-fomokaro <faith.omokaro@andela.com>
2018-12-27 08:54:31 +0100

    add google credentials

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const authRoutes = require('./routes/auth-routes');
 
 const app = express();
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
+
 passport.use(
   new GoogleStrategy({
   //options for the google strategy
-}), () => {
+  callbackURL: process.env.CALLBACK_URL,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+}, () => {
   //passport callback function
   })
 )
